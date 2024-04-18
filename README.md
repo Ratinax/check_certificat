6eff5119f191ff67771200cf7a0ee80b178c1ce2
tbernot <tbernot@hackuity.io>
2024-04-18 10:44:11 +0200

    u[date to handle certificate specification

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # check_certificate
 
-A simple python program used to check ssl/tls certificate
-To test it, just change ip_address and port to your own vallues and do python3 main.py
+A simple python program used to check ssl/tls certificate of a server / proxy
+To test it, just change ip_address, port and cert_file to your own vallues and do
+```
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import ssl
 import socket
 
-def check_certificate(ip_address, port=443):
-    try:
-        context = ssl.create_default_context()
-        with socket.create_connection((ip_address, port)) as sock:
-            with context.wrap_socket(sock, server_hostname=ip_address) as ssock:
-                cert = ssock.getpeercert()
+def check_certificate(ip_address, port, cafile=None):
+	try:
+		context = ssl.create_default_context(cafile=cafile)
+		with socket.create_connection((ip_address, port)) as sock:
+			with context.wrap_socket(sock, server_hostname=ip_address) as ssock:
+				cert = ssock.getpeercert()
+		cert_valid = cert and ('issuer' in cert) and ('subject' in cert)
+		if cert_valid:
+			return True
+		else:
+			return False
+	except Exception as e:
+		return False
 
-        cert_valid = cert and ('issuer' in cert) and ('subject' in cert)
-
-        if cert_valid:
-            print("Certificat not valid")
-        else:
-            print("Certificat no valid")
-
-    except Exception as e:
-        print(f"Erreur while checking certificat validation: {e}")
-
-check_certificate('127.0.0.1')
+print(check_certificate('127.0.0.1', 8080, 'cert.pem'))
